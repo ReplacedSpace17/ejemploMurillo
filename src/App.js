99fb46b38bc16cbb0c06420489787618be6ccd80
ReplacedSpace17 <replacedspace17@gmail.com>
2023-05-25 23:11:39 -0600

    dsa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Targeta from './Tarjeta';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import firebaseConfig from './firebaseConfig';
+
+const App = () => {
+  const [data, setData] = useState('');
+  const [data2, setData2] = useState('');
+
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    const database = firebase.database().ref('/tec');
+
+    const database2 = firebase.database().ref('/testsensor');
+    /*nodo 1*/
+    database.on('value', (snapshot) => {
+      const value = snapshot.val();
+      setData(value);
+    });
+      /*nodo 2*/
+      database2.on('value', (snapshot) => {
+        const value = snapshot.val();
+        setData2(value);
+      });
+
+    return () => {
+      database.off();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="App">
+
+<div className='contTitle'>
+<h1>Ejemplo de React</h1>
+</div>
+<div className='contTarget'>
+  <Targeta texto={data} />
+  <Targeta texto={data2} />
+</div>
+
+</div>
   );
-}
+};
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// firebaseConfig.js
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBd79QAakkjhNtTbZUKWKCSRKfQ2gqn-_o",
+    authDomain: "pruebas-36eeb.firebaseapp.com",
+    databaseURL: "https://pruebas-36eeb-default-rtdb.firebaseio.com",
+    projectId: "pruebas-36eeb",
+    storageBucket: "pruebas-36eeb.appspot.com",
+    messagingSenderId: "988122297370",
+    appId: "1:988122297370:web:45506af7b8ff0fa34a4da3",
+    measurementId: "G-GWE4MWTX15"
+  };
+  
+  export default firebaseConfig;
+  
